bf8e169fd7fad9714fab87b09c6e6d4006a8a452
guneycansanli <guneycansanli@gmail.com>
2023-11-14 22:06:47 -0500

    third lesson math

---
+++ math.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# expr 30 + 10
+# expr 30 - 10
+# expr 30 / 10
+
+# expr 100 * 4
+# expr: syntax error: unexpected argument ‘xxxxx’
+# * is wild card in bash , * is bash is everything like ls -lth /home/*
+
+# expr 100 \* 4 We need to use '\'' escape charater
+
+echo "------------------"
+
+ expr 30 + 10
+ expr 30 - 10
+ expr 30 / 10
+ expr 30 \* 10
+
+echo "------------------"
+
+ mynum1=100
+ mynum2=50
+
+ expr $mynum1 + $mynum2
+ expr $mynum1 - $mynum2
+ expr $mynum1 / $mynum2
+ expr $mynum1 \* $mynum2
+
+echo "------------------"
+echo -e "$mynum1 \n$mynum2"
