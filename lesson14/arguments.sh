e4828489c009cd1e50ea2c41f35c76af1cb1e213
guneycansanli <guneycansanli@gmail.com>
2023-11-29 19:31:57 -0500

    lesson 13 arguments

---
+++ lesson14/arguments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ehco "You entered the argument: $1 ,$2 ,$3, $4 and $5 "
+
+# ./arguments Linux Pizza test car nice
+# ./arguments 4 3 2 1 0
---
+++ lesson14/arguments2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ls -lth $1
+
+#./arguments2.sh /etc/
+#./arguments2.sh /home/
