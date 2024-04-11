a77f5f85e0ea91609e41852cab739c7f793d7111
guneycansanli <guneycansanli@gmail.com>
2024-04-10 23:08:11 -0400

    reading lines with i/o

---
+++ projects/advanced-scripts/reading-lines.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+: <<'COMMENTS'
+This script will be an example for I/O direction and using code of block in bash script {}
+Let's read lines from fstab file.
+COMMENTS
+
+file=/etc/fstab
+
+{
+    read line1
+    read line2
+} <$file
+
+echo "First line of $file is:"
+echo "$line1"
+echo
+echo "Second line of $file is:"
+echo "$line2"
+
+exit 0
+
+# Now, how do you parse the separate fields of each line?
+# Hint: use awk, or . . .
+# . . . Hans-Joerg Diers suggests using the "set" Bash builtin.
